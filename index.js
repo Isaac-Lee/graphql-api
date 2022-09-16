c53d9171c2988f231c79e30959c80ab8dbc0512a
Isaac-Lee <isaac.lee.3412@gmail.com>
2022-09-16 22:00:52 +0900

    init commit

---
+++ index.js
@@ -0,0 +1,13 @@
+import { ApolloServer, gql  } from "apollo-server";
+
+const typeDefs = gql`
+    type Query {
+        text: String
+        hello: String
+    }
+`;
+const server = new ApolloServer({ typeDefs });
+
+server.listen().then(({ url }) => {
+    console.log(`🚀  Server ready at ${url}`);
+});
